d8ad229fec043c06f09f67b11af7f0e2b7536ce5
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-09 20:23:28 -0400

    Made Trip class stub and playing around with controllers/views

---
+++ TrailLocker/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrailLocker.Models
+{
+    public class Trip
+    {
+        //Instance Variables
+        public float current_weight { get; private set; }
+
+        public Trip()
+        {
+            current_weight = 0;
+        }
+    }
+}
