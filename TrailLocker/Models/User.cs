c449c5d2f78202a62a830c84078af74a33998c2d
Colin <colin_akard@gmail.com>
2013-09-10 16:12:21 -0400

    Added User class

---
+++ TrailLocker/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrailLocker.Models
+{
+    public class User
+    {
+        private String name { get; private set; }
+        private String home { get; private set; }
+        public int maxWeight { get; private set; }
+
+        private Locker locker;
+        private List<Trip> trips;
+
+        public User(String name, int maxWeight)
+        {
+            this.name = name;
+            this.maxWeight = maxWeight;
+        }
+    }
+}
