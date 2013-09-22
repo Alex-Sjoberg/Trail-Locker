6a5cbb086fc574fda53d0c35dba6930cec585c9f
Ryan Szemplinski <rszempli@john.cse.taylor.edu>
2013-09-22 15:23:52 -0400

    Added some items stuff

---
+++ TrailLocker/Models/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrailLocker.Models
+{
+    public class Item
+    {
+        public string condition { get; set; }
+        public string location { get; set; }
+        public double weight { get; set; }
+        public double volume { get; set; }
+        public string color { get; set; }
+        public string name { get; set; }
+        public int ID { get; set; }
+        public String brand { get; set; }
+    }
+}
