488acf71193584fc58f2e9d3466db34120febfb1
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-23 16:00:35 -0400

    Forgot to save changes to namespace on a couple files

--- TrailLocker/Repository/IRepository.cs
+++ TrailLocker/Repository/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace COS340.TrailLocker.Data
+namespace TrailLocker.Repository
 {
     public interface IRepository<T> : IDisposable 
         where T : class
